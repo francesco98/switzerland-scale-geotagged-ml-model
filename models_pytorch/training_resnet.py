13a16866eaa27b29be746376ba07aa4606a50fd8
Hartmut Keil <hartmut.keil@adnovum.ch>
2022-10-07 11:18:55 +0200

    minor, loading pytorch for testing CUDA

--- models_pytorch/training_resnet.py
+++ models_pytorch/training_resnet.py
@@ -15,9 +15,9 @@ preprocess = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 input_tensor = preprocess(input_image)
-input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the models_pytorch
 
-# move the input and model to GPU for speed if available
+# move the input and models_pytorch to GPU for speed if available
 if torch.cuda.is_available():
     input_batch = input_batch.to('cuda')
     model.to('cuda')
