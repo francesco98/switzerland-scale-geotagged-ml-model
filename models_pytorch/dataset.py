137651e87c1003ae9cd1a57d9e5e06b295c65674
Hartmut Keil <hartmut.keil@adnovum.ch>
2022-10-13 13:06:25 +0200

    Fixed preprocessing removed random horizontal flip

--- models_pytorch/dataset.py
+++ models_pytorch/dataset.py
@@ -37,7 +37,6 @@ class ImageGeolocationDataset(torch.utils.data.Dataset):
         self.transforms = transforms.Compose([
             transforms.Resize(224),
             transforms.RandomCrop(224),
-            transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
