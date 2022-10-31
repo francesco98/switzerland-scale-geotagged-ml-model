b554f87864a66bcb5ab52edecc0a8aa67474982a
Hartmut Keil <hartmut.keil@adnovum.ch>
2022-10-31 15:54:01 +0100

    separated grid creation and data labeling

--- grid_builder/label_images.py
+++ grid_builder/label_images.py
@@ -2,53 +2,103 @@
 import csv
 import os
 
-from GridBuilder import Point, Image, Grid
+import s2geometry.pywraps2 as s2
+from GridBuilder import Point, Image, Grid, convert_images_for_grid
 
-# Coordinates bounding boy switzerland according https://giswiki.hsr.ch/Bounding_Box
 from grid_builder.flickr_search_images import read_cvs_file, read_excluded_file, read_validated_file
 
-LOWER_BOUND = Point(45.6755, 5.7349)
-UPPER_BOUND = Point(47.9163, 10.6677)
 
-MIN_POINTS = 100
-MAX_POINTS = 1000
+class LabelHelper:
 
+    def __init__(self, file_name: str=None):
+
+        self.__Labels = []
+        self.__all_labels = set()
+        self.__all_cellIds = set()
+
+        if file_name is None:
+            file_name = os.path.dirname(os.path.realpath(__file__))+'/output/grid_cellIds.csv'
+
+        with open(file_name, 'r', encoding='UTF8', newline='') as file:
+            reader = csv.reader(file)
+
+            for tokens in reader:
+
+                if len(tokens) == 0 or tokens[0].startswith('#'):
+                    continue
+
+                label = tokens[0]
+                id = tokens[1]
+
+                if label in self.__all_labels:
+                    raise RuntimeError(f'Duplicated label {label}')
+
+                if id in self.__all_cellIds:
+                    raise RuntimeError(f'Duplicated cellId {id}')
+
+                self.__all_labels.add(label)
+                self.__all_cellIds.add(id)
+
+                cellId = s2.S2CellId(int(id))
+
+                self.__Labels.append((label, cellId))
+
+    def get_label(self, image: Image) -> str:
+
+        for label, cell in self.__Labels:
+
+            if cell.contains(image.coords.cellId):
+                return label
+
+        return None
+
+    def get_cellId(self, data_label: str) -> s2.S2CellId:
+
+        for label, cell in self.__Labels:
+
+            if label == data_label:
+                return cell
+
+        return None
 
 
 def create_labels(dataset_name):
 
-    images = read_cvs_file(f'input/{dataset_name}.csv')
-    excluded = read_excluded_file(f'input/{dataset_name}_excluded.csv')
-    validated = read_validated_file(f'input/{dataset_name}_validated.csv')
+    # read label files
+    label_helper = LabelHelper()
 
-    output_grid_file = 'output/' + dataset_name + "_grid.csv"
+    # read and convert datapoints
+    data_points = convert_images_for_grid(dataset_name)
+
+    # get labels fro the datapoints
+    for image in data_points:
+        label = label_helper.get_label(image)
+        image.label = label
+
     output_label_file = 'output/' + dataset_name + "_label.csv"
-
-    data_points = []
-
-    for id in images:
-
-        if id in excluded:
-            continue
-
-        if id not in validated:
-            continue
-
-        elem = images[id]
-        url = elem['url']
-        longitude = float(elem['longitude'])
-        latitude = float(elem['latitude'])
-
-        point = Point(latitude, longitude)
-        data_points.append(Image(id, url, point))
-
-
-    grid = Grid(data_points, LOWER_BOUND, UPPER_BOUND, MIN_POINTS, MAX_POINTS)
-    grid.buildGrid()
-    grid.toWkt(output_grid_file)
-
     header = ['# ID', 'Label']
-    labels = [[data.id, data.label] for data in data_points if data.label != None]
+    labels = []
+    labels_map = {}
+
+    for data in data_points:
+        if data.label is None:
+            continue
+
+        if data.label not in labels_map:
+            labels_map[data.label] = []
+
+        labels.append([data.id, data.label])
+        labels_map[data.label].append(data.id)
+
+    print(f'Labeled {len(labels)} of {len(data_points)} datapoints ({len(data_points) - len(labels)} missed)')
+
+    for label in labels_map:
+        cellId = label_helper.get_cellId(label)
+        id = cellId.id()
+        face = cellId.face()
+        level = cellId.level()
+        datas = labels_map[label]
+        print(f'Label: {label} (id: {id} level: {level} face: {face}) has \t\t {len(datas)} datapoints')
 
     with open(output_label_file, 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
